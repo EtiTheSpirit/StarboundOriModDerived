a5f1293e529dc43127e2858b0793a8e70579a4f0
BuildTools <unconfigured@null.spigotmc.org>
2019-03-21 19:26:07 -0500

    Add some special songs and fix up some events.

--- stats/effects/spiritguardianpassiveevt/passiveevents.lua
+++ stats/effects/spiritguardianpassiveevt/passiveevents.lua
@@ -21,34 +21,48 @@ end
 function CurrentDate(z)
 	--Credits to https://scriptinghelpers.org/questions/25121/how-do-you-get-the-date-and-time-from-unix-epoch-time
 	local z = z or os.time()
-    local z = math.floor(z / 86400) + 719468
-    local era = math.floor(z / 146097)
-    local doe = math.floor(z - era * 146097)
-    local yoe = math.floor((doe - doe / 1460 + doe / 36524 - doe / 146096) / 365)
-    local y = math.floor(yoe + era * 400)
-    local doy = doe - math.floor((365 * yoe + yoe / 4 - yoe / 100))
-    local mp = math.floor((5 * doy + 2) / 153)
-    local d = math.ceil(doy - (153 * mp + 2) / 5 + 1)
-    local m = math.floor(mp + (mp < 10 and 3 or -9))
-    return y + (m <= 2 and 1 or 0), m, d
+	local z = math.floor(z / 86400) + 719468
+	local era = math.floor(z / 146097)
+	local doe = math.floor(z - era * 146097)
+	local yoe = math.floor((doe - doe / 1460 + doe / 36524 - doe / 146096) / 365)
+	local y = math.floor(yoe + era * 400)
+	local doy = doe - math.floor((365 * yoe + yoe / 4 - yoe / 100))
+	local mp = math.floor((5 * doy + 2) / 153)
+	local d = math.ceil(doy - (153 * mp + 2) / 5 + 1)
+	local m = math.floor(mp + (mp < 10 and 3 or -9))
+	return y + (m <= 2 and 1 or 0), m, d
+end
+
+function SayAsSAIL(Message)
+	local RadioMessage = {
+		type="tutorial",
+		text=Message,
+		unique=false,
+	}
+	world.sendEntityMessage(entity.uniqueId(), "radioMessage", RadioMessage) --Added by questAccessDeployment.lua in scripts
 end
 
 function OriBirthdayFunction(IsBirthday)
-	if IsBirthday then
-		-- local Year, Month, Day = CurrentDate()
-		-- local OriYear = tostring(Year - 2015)
-		-- local RadioMessage = {type="mission", 
-			-- text="I just thought I'd let you know that today is Ori's " .. OriYear .. GetNumberEnding(OriYear) .. " birthday."
-		-- }
-		-- world.sendEntityMessage(entity.uniqueId(), "radioMessage", RadioMessage)
-		if world.sendEntityMessage(entity.uniqueId(), "hasCompletedQuest", "march11") then return end
-		world.sendEntityMessage(entity.uniqueId(), "startQuest", "march11") --Added by questAccessDeployment.lua in scripts
-	end 
+	if not IsBirthday then return end
+	local Year, Month, Day = CurrentDate()
+	local OriYear = tostring(Year - 2015)
+	--SayAsSAIL("I just thought I'd let you know that today is Ori's " .. OriYear .. GetNumberEnding(OriYear) .. " birthday. I've taken the liberty of sending a virtual postcard for you. Thank me later.")
+	if world.sendEntityMessage(entity.uniqueId(), "hasCompletedQuest", "march11"):result() then return end
+	world.sendEntityMessage(entity.uniqueId(), "startQuest", "march11") --Added by questAccessDeployment.lua in scripts
+end
+
+function XanBirthdayFunction(IsBirthday)
+	if not IsBirthday then return end
+	local Year, Month, Day = CurrentDate()
+	local XanYear = tostring(Year - 2000)
+	world.sendEntityMessage(entity.uniqueId(), "radioMessage", "xanBirthdayRadioMessage")
+	--SayAsSAIL("Today's a bit special. It's Xan's " .. XanYear .. GetNumberEnding(XanYear) .. " birthday. You probably don't know who he is, though. He was one of the first people to show interest in your culture within The Protectorate.")
 end
 
 function init()
 	local Year, Month, Day = CurrentDate()
 	self.IsOrisBirthday = (Month == 3 and Day == 11)
+	self.IsXansBirthday = (Month == 3 and Day == 21) --xan did 7/11 >:(. Set to today for testing shit.
 	
 	self.hasDoneStuff = false
 end
@@ -57,7 +71,9 @@ function update(dt)
 	if not self.hasDoneStuff then 
 		self.hasDoneStuff = true
 		OriBirthdayFunction(self.IsOrisBirthday)
-	end 
+		XanBirthdayFunction(self.IsXansBirthday)
+		status.clearAllPersistentEffects()
+	end
 end
 
 function uninit()
