ea2e74f06c01b8be385ab4f1afdbd3040919c175
BuildTools <unconfigured@null.spigotmc.org>
2019-03-21 20:05:06 -0500

    Removed the stock radio messages and appended better ones into passivevents.lua

--- stats/effects/spiritguardianpassiveevt/passiveevents.lua
+++ stats/effects/spiritguardianpassiveevt/passiveevents.lua
@@ -33,11 +33,12 @@ function CurrentDate(z)
 	return y + (m <= 2 and 1 or 0), m, d
 end
 
-function SayAsSAIL(Message)
+function SayAsSAIL(Message, MessageId)
 	local RadioMessage = {
-		type="tutorial",
+		type="mission",
 		text=Message,
 		unique=false,
+		messageId=MessageId or tostring(math.random())
 	}
 	world.sendEntityMessage(entity.uniqueId(), "radioMessage", RadioMessage) --Added by questAccessDeployment.lua in scripts
 end
@@ -46,23 +47,23 @@ function OriBirthdayFunction(IsBirthday)
 	if not IsBirthday then return end
 	local Year, Month, Day = CurrentDate()
 	local OriYear = tostring(Year - 2015)
-	--SayAsSAIL("I just thought I'd let you know that today is Ori's " .. OriYear .. GetNumberEnding(OriYear) .. " birthday. I've taken the liberty of sending a virtual postcard for you. Thank me later.")
-	if world.sendEntityMessage(entity.uniqueId(), "hasCompletedQuest", "march11"):result() then return end
-	world.sendEntityMessage(entity.uniqueId(), "startQuest", "march11") --Added by questAccessDeployment.lua in scripts
+	SayAsSAIL("I just thought I'd let you know that today is ^cyan;Ori^reset;'s ^green;" .. OriYear .. GetNumberEnding(OriYear) .. "^reset; birthday. I've taken the liberty of sending a virtual postcard for you. You can thank me later.", "OriBirthday"..OriYear)
+	-- if world.sendEntityMessage(entity.uniqueId(), "hasCompletedQuest", "march11"):result() then return end
+	-- world.sendEntityMessage(entity.uniqueId(), "startQuest", "march11") --Added by questAccessDeployment.lua in scripts
 end
 
 function XanBirthdayFunction(IsBirthday)
 	if not IsBirthday then return end
 	local Year, Month, Day = CurrentDate()
-	local XanYear = tostring(Year - 2000)
-	world.sendEntityMessage(entity.uniqueId(), "radioMessage", "xanBirthdayRadioMessage")
-	--SayAsSAIL("Today's a bit special. It's Xan's " .. XanYear .. GetNumberEnding(XanYear) .. " birthday. You probably don't know who he is, though. He was one of the first people to show interest in your culture within The Protectorate.")
+	local XanYear = tostring(Year - 1626)
+	local ActualXanYear = tostring(Year - 2000)
+	SayAsSAIL("Today's is ^#FF3F00;Xan^reset;'s ^green;" .. XanYear .. GetNumberEnding(XanYear) .. "^reset; birthday ^darkgray;(" .. ActualXanYear .. GetNumberEnding(ActualXanYear) .. " birthday in real life)^reset;. You probably don't know who he is. He's the Draconian who found the ^cyan;Spirit Guardian^reset; homeworld, ^cornflowerblue;Nibel^reset;, and suggested your race be annexed into ^yellow;The Protectorate^reset;. I thought I'd let you know.", "XanBirthday"..ActualXanYear)
 end
 
 function init()
 	local Year, Month, Day = CurrentDate()
 	self.IsOrisBirthday = (Month == 3 and Day == 11)
-	self.IsXansBirthday = (Month == 3 and Day == 21) --xan did 7/11 >:(. Set to today for testing shit.
+	self.IsXansBirthday = (Month == 7 and Day == 11) --xan did 7/11 >:(
 	
 	self.hasDoneStuff = false
 end
@@ -72,7 +73,6 @@ function update(dt)
 		self.hasDoneStuff = true
 		OriBirthdayFunction(self.IsOrisBirthday)
 		XanBirthdayFunction(self.IsXansBirthday)
-		status.clearAllPersistentEffects()
 	end
 end
 
